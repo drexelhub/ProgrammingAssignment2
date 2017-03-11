ebc1791e96c62867e75ee165fc2740a5a3f4977f
drexelhub <sethshome@gmail.com>
2017-03-10 19:24:07 -0500

    Updated with function bodies
    Added the function bodies to the skeleton

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions will create and cache an inverse of a matrix or retrieve the cached version of an
+## inverse matrix if it is already cached
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function will create an object to cache a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      setinv <- function(inverse) inv <<- inverse
+      getinv <- function() inv
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## the cacheSolve will retrieve a cached version of an inverse matrix or create and cache the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinv()
+     
+      if(!is.null(inv)) {
+            message("The Matrix is already cached")
+            return(inv)
+      }
+      
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$setinv(inv)
+      inv
 }
